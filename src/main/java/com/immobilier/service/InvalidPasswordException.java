1c33ec2ecbbce9ae8022196caa01193908cd0ab7
Micipsa <missi.adel@gmail.com>
2022-12-24 14:23:49 +0100

    initialize

---
+++ src/main/java/com/immobilier/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package com.immobilier.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}
